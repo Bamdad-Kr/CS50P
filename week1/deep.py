52a5234394e125b7ec2b133a3597da8c8cf656e7
Bamdad-Kr <149760246+Bamdad-Kr@users.noreply.github.com>
2024-09-17 18:55:16 +0330

    Create deep.py

---
+++ week1/deep.py
@@ -0,0 +1,6 @@
+m = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").lower().strip()
+my_list = ["forty-two", "42", "forty two"]
+if m in my_list:
+    print("Yes")
+else:
+    print("No")
